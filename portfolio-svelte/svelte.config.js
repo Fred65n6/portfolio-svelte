d3bf66974328a39e8b126ca0e96b797b3df4f78c
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2022-03-30 10:09:28 +0200

    netlify

--- portfolio-svelte/svelte.config.js
+++ portfolio-svelte/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,4 +12,8 @@ const config = {
 	}
 };
 
-export default config;
+export default {
+	kit: {
+		adapter: adapter()
+	}
+};
