cb15e570a8b062691c6268c39b699df0656ecef2
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2022-05-03 20:35:00 +0200

    no animation

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -3,8 +3,8 @@
 
 <div class="hero md:bg-pan-left">
 	<div class="grid place-items-center md:border-none ">
-		<img class="fade-in " src="./frederik_milland.svg" alt="logo" />
-		<h1 class="md:tracking-in-expand-fwd fade-in">WEBDESIGNER</h1>
+		<img class="md:fade-in " src="./frederik_milland.svg" alt="logo" />
+		<h1 class="md:tracking-in-expand-fwd ">WEBDESIGNER</h1>
 		<a class="pulsate-fwd" href="#forside">︾</a>
 	</div>
 </div>
